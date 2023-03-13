ca40b3e0ba49762de227c1558a5e732bdccccd66
janaercole <janaercole@gmail.com>
2023-03-12 22:15:06 -0300

    front ajustes

--- app/src/main/java/com/example/tccfrontmobileusuario/MainActivity.java
+++ app/src/main/java/com/example/tccfrontmobileusuario/MainActivity.java
@@ -2,7 +2,9 @@ package com.example.tccfrontmobileusuario;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -11,4 +13,17 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
     }
+
+
+    public void primeiroAcesso(View view){
+        Intent it = new Intent( this, PrimeiroAcesso.class);
+        startActivity(it);
+
+
+
+    }
+
+
+
+
 }
