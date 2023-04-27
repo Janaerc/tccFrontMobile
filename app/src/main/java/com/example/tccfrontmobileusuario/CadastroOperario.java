b0d728cc251c94ac8afdc03d60638b665d1992ab
janaercole <janaercole@gmail.com>
2023-04-26 22:03:45 -0300

    telas operario chamado home e cadastro

---
+++ app/src/main/java/com/example/tccfrontmobileusuario/CadastroOperario.java
@@ -0,0 +1,14 @@
+package com.example.tccfrontmobileusuario;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class CadastroOperario extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_cadastro_operario);
+    }
+}
