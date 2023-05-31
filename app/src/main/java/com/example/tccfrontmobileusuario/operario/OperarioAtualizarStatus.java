baaf7d1a1422d11f9aa75217626c4310f323cf22
janaercole <janaercole@gmail.com>
2023-05-30 21:47:32 -0300

    tela atualizar status

---
+++ app/src/main/java/com/example/tccfrontmobileusuario/operario/OperarioAtualizarStatus.java
@@ -0,0 +1,16 @@
+package com.example.tccfrontmobileusuario.operario;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.example.tccfrontmobileusuario.R;
+
+public class OperarioAtualizarStatus extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_operario_atualizar_status);
+    }
+}
