3cf4520ef84f591fc61b35b7715cc445cd6bad0e
RafaelTombolato <rafaelprestes1002@gmail.com>
2023-07-04 21:46:00 -0300

    redirecionamento usuario

--- app/src/main/java/com/example/tccfrontmobileusuario/usuario/HomepageUsuario.java
+++ app/src/main/java/com/example/tccfrontmobileusuario/usuario/HomepageUsuario.java
@@ -90,19 +90,22 @@ public class HomepageUsuario extends AppCompatActivity {
                                     params.putSerializable("chamado", selectedChamadoDTO);
                                     System.out.println("aqui está o chamado que veio do banco");
                                     System.out.println(selectedChamadoDTO);
-                                    if(selectedChamadoDTO.getStatusId().getId() == 1 && selectedChamadoDTO.getOrdemServicoId().getUsuarioOperarioId().getId() != null) {
+                                    if(selectedChamadoDTO.getStatusId().getId() == 3) {
+                                        System.out.println("entrou no 1");
                                         Intent it = new Intent(HomepageUsuario.this, DetalhesChamadoEmAndamento.class);
                                         it.putExtra("chamado", selectedChamadoDTO);
                                         it.putExtra("usuario", usuarioDTO);
                                         startActivity(it);
                                     }
-                                    if(selectedChamadoDTO.getStatusId().getId() == 1 && selectedChamadoDTO.getOrdemServicoId().getUsuarioOperarioId().getId() == null) {
+                                    if(selectedChamadoDTO.getStatusId().getId() == 1) {
+                                        System.out.println("entrou no 2");
                                         Intent it = new Intent(HomepageUsuario.this, DetalhesChamadoAberto.class);
                                         it.putExtra("chamado", selectedChamadoDTO);
                                         it.putExtra("usuario", usuarioDTO);
                                         startActivity(it);
                                     }
                                     if(selectedChamadoDTO.getStatusId().getId() == 2) {
+                                        System.out.println("entrou no 3");
                                         Intent it = new Intent(HomepageUsuario.this, DetalhesChamadoEncerrado.class);
                                         it.putExtra("chamado", selectedChamadoDTO);
                                         it.putExtra("usuario", usuarioDTO);
